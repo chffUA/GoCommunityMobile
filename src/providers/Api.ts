a642b759abd08f4ee2a6d357e995fddc596218f6
chffUA <32465991+chffUA@users.noreply.github.com>
2018-05-30 00:20:22 +0100

    Update Api.ts

--- src/providers/Api.ts
+++ src/providers/Api.ts
@@ -12,7 +12,7 @@ export class Api {
     constructor(public http: Http) {
         //console.log('Provider online');
         this.header = new Headers();
-        this.header.append('Content-Type', 'text/plain');
+        this.header.append('Content-Type', 'application/json');
         //this.header.append('Access-Control-Allow-Origin', '*');
         //this.header.append('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT');
     }
